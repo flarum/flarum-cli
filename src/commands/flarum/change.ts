815426423cf6a9a6eb0fd6ad876bffdf37393f8d
Sami Mazouz <sychocouldy@gmail.com>
2024-11-27 20:32:47 +0100

    chore: yarn format

--- src/commands/flarum/change.ts
+++ src/commands/flarum/change.ts
@@ -1,10 +1,10 @@
-import {Command} from "@oclif/core";
-import path from "path";
-import {PromptsIO} from "boilersmith/io";
-import {exit} from "@oclif/errors";
-import {execSync, exec} from "child_process";
-import util from "util";
-import chalk from "chalk";
+import { Command } from '@oclif/core';
+import path from 'path';
+import { PromptsIO } from 'boilersmith/io';
+import { exit } from '@oclif/errors';
+import { execSync, exec } from 'child_process';
+import util from 'util';
+import chalk from 'chalk';
 
 const execAsync = util.promisify(exec);
 
@@ -14,7 +14,7 @@ export default class Info extends Command {
   /**
    * Should only need to keep two registered at all times.
    */
-  static map: {cli:string|number,flarum:string|number}[] = [
+  static map: { cli: string | number; flarum: string | number }[] = [
     { cli: 3, flarum: 2 },
     { cli: 2, flarum: 1 },
   ];
@@ -26,9 +26,9 @@ export default class Info extends Command {
     const currentFlarumVersion = Number(json.flarum.split('.')[0]);
     const currentCliVersion = Number(json.version.split('.')[0]);
 
-    const other = Info.map.find(i => i.cli !== currentCliVersion);
+    const other = Info.map.find((i) => i.cli !== currentCliVersion);
 
-    if (! other) {
+    if (!other) {
       this.error('No other versions to switch to.');
     }
 
@@ -53,7 +53,7 @@ export default class Info extends Command {
       initial: true,
     });
 
-    if (! yes) {
+    if (!yes) {
       return;
     }
 
@@ -63,11 +63,11 @@ export default class Info extends Command {
     const otherBin = `fl${otherFlarumVersion}`;
     let otherBinPath = this.binPath(otherBin);
 
-    if (! otherBinPath) {
+    if (!otherBinPath) {
       this.install(otherCliVersion, otherFlarumVersion);
       otherBinPath = this.binPath(otherBin);
 
-      if (! otherBinPath) {
+      if (!otherBinPath) {
         this.error('Could not install the other version.');
       }
     }
@@ -75,18 +75,18 @@ export default class Info extends Command {
     const defaultBinPath = otherBinPath.split('/').slice(0, -1).join('/') + '/fl';
 
     if (!this.isWindows()) {
-      const which = execSync('which '+otherBin, { encoding: 'utf8' }).trim();
+      const which = execSync('which ' + otherBin, { encoding: 'utf8' }).trim();
       // replace the default fl bin with the now installed fl2 or fl1 bin
       // the path to the fl bin is the same as the path to the fl2 or fl1 bin, just without the 2 or 1
       this.execRetryingAsSudo(`ln -sf ${which} ${defaultBinPath}`);
     } else {
-      const where = execSync('where '+otherBin, { encoding: 'utf8' }).trim();
+      const where = execSync('where ' + otherBin, { encoding: 'utf8' }).trim();
       execSync(`mklink /H C:\\Windows\\System32\\fl.exe ${where}`, { stdio: 'inherit' });
     }
   }
 
-  install(cli: number|string, flarum: number|string, sudo = false) {
-    execAsync((sudo ? 'sudo ' : '') + `npm install -g fl${flarum}@npm:@flarum/cli@${cli} --force`).then(({stdout, stderr}) => {
+  install(cli: number | string, flarum: number | string, sudo = false) {
+    execAsync((sudo ? 'sudo ' : '') + `npm install -g fl${flarum}@npm:@flarum/cli@${cli} --force`).then(({ stdout }) => {
       if (stdout && stdout.toString().includes('Permission denied') && !sudo && !this.isWindows()) {
         this.log(chalk.red('Permission denied. Running with sudo.'));
         this.install(cli, flarum, true);
@@ -98,10 +98,8 @@ export default class Info extends Command {
     return process.platform === 'win32';
   }
 
-  binPath(bin: string): null|string {
-    const result = this.isWindows()
-      ? execSync('where '+bin, { encoding: 'utf8' }).trim()
-      : execSync('which '+bin, { encoding: 'utf8' }).trim();
+  binPath(bin: string): null | string {
+    const result = this.isWindows() ? execSync('where ' + bin, { encoding: 'utf8' }).trim() : execSync('which ' + bin, { encoding: 'utf8' }).trim();
 
     this.log(result);
 
@@ -109,10 +107,10 @@ export default class Info extends Command {
   }
 
   protected execRetryingAsSudo(command: string): void {
-    execAsync(command).catch(({stderr}) => {
+    execAsync(command).catch(({ stderr }) => {
       if (stderr && stderr.toString().includes('Permission denied')) {
         this.log(chalk.red('Permission denied. Running with sudo.'));
-        execSync('sudo '+command, { stdio: 'inherit' });
+        execSync('sudo ' + command, { stdio: 'inherit' });
       }
     });
   }
